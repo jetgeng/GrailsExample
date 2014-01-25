d12948ac4b316c68819aebca0b96e4efa9feeed6
jetgeng <jetgeng@gmail.com>
2014-01-25 15:02:56 +0800

    完成初始化

---
+++ JetGrailsSecurityExample/grails-app/conf/BootStrap.groovy
@@ -0,0 +1,11 @@
+class BootStrap {
+
+    def init = { servletContext ->
+        // new RequestMap(url: '/user/**', configAttribute: 'ROLE_USER').save()
+        // new RequestMap(url: '/requestmap/**', configAttribute: 'ROLE_USER').save()
+        // new RequestMap(url: '/role/**', configAttribute: 'ROLE_USER').save()
+
+    }
+    def destroy = {
+    }
+}
---
+++ JetGrailsSecurityExample/grails-app/conf/DataSource.groovy
@@ -0,0 +1,47 @@
+dataSource {
+    pooled = true
+    logSql = true
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "yl"
+    password = "summary"
+
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = false
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+//    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+}
+
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/grails_security?useUnicode=true&characterEncoding=utf8"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/grails_security?useUnicode=true&characterEncoding=utf8"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:mysql://localhost/grails_security?useUnicode=true&characterEncoding=utf8"
+            properties {
+                maxActive = -1
+                minEvictableIdleTimeMillis = 1800000
+                timeBetweenEvictionRunsMillis = 1800000
+                numTestsPerEvictionRun = 3
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                validationQuery = "SELECT 1"
+                jdbcInterceptors = "ConnectionState"
+            }
+        }
+    }
+}
